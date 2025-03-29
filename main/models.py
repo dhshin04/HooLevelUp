c2c8b1e018a04924aef180a2af1cefa44e806864
DJPark1213 <tfv4cm@virginia.edu>
2025-03-29 11:09:41 -0400

    skill

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from skills.models import Skill
 
 # ---------- Items ----------
 class Item(models.Model):
--- schedule/models.py
+++ schedule/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from skills.models import Skill
 
 # Create your models here.
 class Scheduler(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    goals = models.ManyToManyField(Goal, blank=True)
+    skills = models.ManyToManyField(Skill, blank=True)
     date = models.DateField()
     time = models.TimeField(null=True, blank=True)
