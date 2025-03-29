c057069522ffcc6daeb8ef64fc0a0f7428ca48e2
DJPark1213 <tfv4cm@virginia.edu>
2025-03-29 11:50:00 -0400

    url change

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,7 @@ app_name = 'main'
 
 urlpatterns = [
     path('', views.main_screen, name='main_screen'),
-    path('accounts/',views.accounts,name='accounts'),
-    path('setup/', views.setup, name='setup'),
-    path('animals/<str:identifier>/', views.animals, name='animals'),
+    # path('accounts/',views.accounts,name='accounts'),
+    # path('setup/', views.setup, name='setup'),
+    # path('animals/<str:identifier>/', views.animals, name='animals'),
 ]
