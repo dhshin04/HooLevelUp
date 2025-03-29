024605ba0c8dcd582020ace5bb3ad06f4691e87d
Donghwa Shin <68676833+dhshin04@users.noreply.github.com>
2025-03-29 10:56:41 -0400

    Finished initial setup with Postgres

--- hoolevelup/settings.py
+++ hoolevelup/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
+    'skills',
+    'schedule',
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -73,10 +77,18 @@ WSGI_APPLICATION = 'hoolevelup.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'HooLevelUp',
+        'USER': os.environ['USER'],
+        'PASSWORD': os.environ['PASSWORD'],
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -105,7 +117,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
